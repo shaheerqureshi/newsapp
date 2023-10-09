5ca08044cab8b83ad79090fba19c9b69d2057bc2
shaheerqureshi <shaheerqureshi334@gmail.com>
2023-10-08 23:54:21 -0400

    News Card Added with sample data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello from class based component
+        <Navbar/>
+        <News/>
       </div>
     )
   }

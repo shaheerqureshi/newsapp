46a77c58976e3ad5bd226e1be05a7541d70b365e
shaheerqureshi <shaheerqureshi334@gmail.com>
2023-10-17 05:23:34 -0400

    Spinner added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor(){
@@ -11,32 +12,35 @@ export class News extends Component {
     }
   }
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cf14b702fcc54cebad78c578c6dc8209";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cf14b702fcc54cebad78c578c6dc8209&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults,loading:false})
   }
   handlePrevClick = async () =>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cf14b702fcc54cebad78c578c6dc8209&page=${this.state.page - 1}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cf14b702fcc54cebad78c578c6dc8209&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page:this.state.page-1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
+      loading:false
     })
 
   }
   handleNextClick = async () =>{
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)) {
-      
-    }else{
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cf14b702fcc54cebad78c578c6dc8209&page=${this.state.page + 1}&pageSize=20`;
+    if (!this.state.page + 1 > Math.ceil(this.state.totalResults/20)) {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cf14b702fcc54cebad78c578c6dc8209&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true})
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page:this.state.page+1,
-        articles: parsedData.articles
+        articles: parsedData.articles,
+        loading:false
       })
     }
   }
@@ -48,7 +52,8 @@ export class News extends Component {
     return (
       
       <div className='container my-3'>
-        <h2>CanNews - Top Headlines</h2>
+        {this.state.loading && <Spinner/>}
+        <h1 className="text-center">CanNews - Top Headlines</h1>
         <div className="row">
         {this.state.articles.map(element=>{
               return <div className="col-md-4" key={element.url}>
@@ -60,7 +65,7 @@ export class News extends Component {
 
         <div className="container d-flex justify-content-between">
           <button type="button" disabled={this.state.page<=1} class="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)} class="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <a target='_blank' rel='noreferrer' href={newsUrl}><img src={imageUrl} className="card-img-top" alt="..." /></a>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+            <a href={newsUrl}  rel='noreferrer'  target='_blank' className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
